b683622370ec28655399d46a35855a73bf99eaf3
Eva Liu <liuyanzheneva@126.com>
2016-08-09 20:45:44 +0800

    new chart class

---
+++ resources/public/TimeseriesChart.js
@@ -0,0 +1,33 @@
+
+Ext.define('Ext.correlationtest.TimeseriesChart',{
+    extend : 'Ext.chart.Chart',
+    xtype : 'timeserieschart',
+    width : 300,
+    height : 300,
+    store: Ext.create('Ext.data.JsonStore', {
+                fields : ['time','num'],
+                data : [],
+                autoLoad : true
+           }),
+
+    axes: [
+                                        {
+                                            title: 'Time',
+                                            type: 'Numeric',
+                                            position: 'bottom'
+                                        },
+                                        {
+                                            title: 'Value',
+                                            type: 'Numeric',
+                                            position: 'left'
+                                        }
+                                    ],
+    series: [
+                                        {
+                                            type: 'line',
+                                            xField: 'time',
+                                            yField: 'num'
+                                        }
+                                    ]
+
+});
